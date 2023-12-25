19e256f15e50fc915366fc291519bd03ec6042a5
AbhiPrabhu0805 <106758150+AbhiPrabhu0805@users.noreply.github.com>
2023-12-25 11:41:44 -0500

    Adding a new python test script for PCEP course

---
+++ PythonCode/module_1.py
@@ -0,0 +1,26 @@
+'''print('What is your name?')
+username=input("Enter your name:")
+print('Hello!', username)
+print("Hello Everyone",\
+      "My name is",username,end='.\n',sep='-')'''
+
+my_list=[1,2,3,4,5]
+print(my_list)
+print(my_list[::-1])
+del my_list[2]
+print(my_list)
+my_list.append(2)
+print(my_list)
+
+my_tuple=(1,2,3,4,5)
+print(my_tuple)
+print(my_tuple[0:3])
+
+dict={}
+dict["John"]='A'
+dict["Brown"]='B'
+print(dict)
+
+for i in dict.values():
+    print(i)
+
